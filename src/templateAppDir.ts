bd4b2868d76389e970826392d340db757ad543f7
Sven Jens <sven@sven-jens.nl>
2024-11-26 13:52:41 +0100

    fix: updated params to await params

--- src/templateAppDir.ts
+++ src/templateAppDir.ts
@@ -99,7 +99,7 @@ function templateRSCPage({
   ${code}
 
   export default async function __Next_Translate_new__${hash}__(props) {
-    const detectedLang = props.params?.lang ?? props.searchParams?.lang
+    const detectedLang = await props.params?.lang ?? await props.searchParams?.lang
 
     if (detectedLang === 'favicon.ico') return <${pageVariableName} {...props} />
 
